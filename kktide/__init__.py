19071c34125c423ed4b28be1373594d226a70772
kazukingh01 <kazukingh01@gmail.com>
2024-01-06 18:35:52 +0900

    modify

---
+++ kktide/__init__.py
@@ -0,0 +1,1 @@
+from kktide import config
---
+++ kktide/config/psgre.py
@@ -0,0 +1,12 @@
+__all__ = [
+    "HOST",
+    "PORT",
+    "USER",
+    "PASS",
+]
+
+HOST = "127.0.0.1"
+PORT = 5432
+USER = "postgres"
+PASS = "postgres"
+DBNAME = "testdb"
